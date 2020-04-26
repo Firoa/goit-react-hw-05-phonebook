fab1557b7a601f7e2573a354c62807dd5f410770
Sasha <in200pac1lab@gmail.com>
2020-04-26 19:06:21 +0300

    Updates

---
+++ precache-manifest.155208095148c3fd7ef507128fa920f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa507c126aa9e815ba88ed65dff7846",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "32bee43be52900f5a623",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.57f74547.chunk.css"
+  },
+  {
+    "revision": "7015eaf9b69350eb277c",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.68e51752.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.68e51752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32bee43be52900f5a623",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.4e3d5bda.chunk.js"
+  },
+  {
+    "revision": "b7b754233b76014dcb08",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.756f41b0.js"
+  }
+]);
